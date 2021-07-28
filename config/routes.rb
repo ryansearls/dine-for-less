a90ae44b10b762bde0c7ecc358d60239459211a5
Ryan Searls <Ryan.searls@gmail.com>
2021-07-28 15:07:06 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/restaurants" => "restaurants#index"
+
+  get "/restaurants/:id" => "restaurants#show"
+
+  post "/restaurants" => "restaurants#create"
+
+  patch "/restaurants/:id" => "restaurants#update"
+
+  delete "/restaurants/:id" => "restaurants#destroy"
+
+  post "/users" => "users#create"
+
+  post "/sessions" => "sessions#create"
+
 end
